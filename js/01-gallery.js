c5ef34b4a86a4d1b50c6fb0de8f3b282c8429eee
dmnfrd <ririkka11@gmail.com>
2023-04-03 17:30:48 +0200

    upd

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,66 @@
+/*Створення і рендер розмітки на підставі масиву даних galleryItems і наданого шаблону елемента галереї.
+Реалізація делегування на ul.gallery і отримання url великого зображення.
+Підключення скрипту і стилів бібліотеки модального вікна basicLightbox. Використовуй CDN сервіс jsdelivr і додай у проект посилання на мініфіковані (.min) файли бібліотеки.
+Відкриття модального вікна по кліку на елементі галереї. Для цього ознайомся з документацією і прикладами.
+Заміна значення атрибута src елемента <img> в модальному вікні перед відкриттям. Використовуй готову розмітку модального вікна із зображенням з прикладів бібліотеки basicLightbox.*/
+
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
+const galleryBox= document.querySelector("ul.gallery");
+const img = createImg('galleryItems');
+galleryBox.insertAdjacentHTML("beforeend", img);
+
+
+/*Посилання на оригінальне зображення повинно зберігатися в data-атрибуті source на елементі <img>, і вказуватися в href посилання. Не додавай інші HTML теги або CSS класи, крім тих, що містяться в цьому шаблоні.
+
+<li class="gallery__item">
+  <a class="gallery__link" href="large-image.jpg">
+    <img
+      class="gallery__image"
+      src="small-image.jpg"
+      data-source="large-image.jpg"
+      alt="Image description"
+    />
+  </a>
+</li>
+
+Зверни увагу на те, що зображення обгорнуте посиланням, отже по кліку за замовчуванням користувач буде перенаправлений на іншу сторінку. Заборони цю поведінку за замовчуванням. */
+
+
+function createImg(img) {
+  return galleryItems
+    .map(({ preview, original, description }) => {
+      return `<div class="gallery__item">
+      <a class="gallery__link" href="${original}">
+        <img
+          class="gallery__image"
+          src="${preview}"
+          data-source="${original}"
+          alt="${description}"
+        />
+      </a>
+    </div>`;
+    })
+    .join("");
+}
+
+/*Створи галерею з можливістю кліку по її елементах і перегляду повнорозмірного зображення у модальному вікні. Подивися демо відео роботи галереї.
+Виконуй це завдання у файлах 01-gallery.html і 01-gallery.js.*/
+
+const onBoxClick = (element) => {
+  element.preventDefault();
+
+  if (element.target.classList.contains("gallery")) return;
+  const source = element.target.dataset.source;
+    
+  const instance = basicLightbox.create(`
+    <img src="${source}" width="800" height="600">`);
+  instance.show();
+};
+
+galleryBox.addEventListener("click", onBoxClick);
+
 console.log(galleryItems);
+
+
